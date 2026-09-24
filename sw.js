3aa09130cd21c626ca15265f674100acb3dfba0a
window-boult <ganganschool999@gmail.com>
2026-09-24 11:53:09 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,20 @@
 const CACHE_NAME = 'sea-app-v1';
 const ASSETS_TO_CACHE = [
-  '/client.html',
+  '/',
+  '/index.html',
   '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
-// Install Event
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
   );
 });
 
-// Fetch Event (Serves cached content when offline)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
